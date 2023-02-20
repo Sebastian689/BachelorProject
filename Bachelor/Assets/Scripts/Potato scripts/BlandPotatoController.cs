20894d70eacb3aa4559b40756c37a62f3664cd91
Simon <mukopper@gmail.com>
2023-02-20 15:13:49 +0100

    Prefapped potato

--- Bachelor/Assets/Scripts/Potato scripts/BlandPotatoController.cs
+++ Bachelor/Assets/Scripts/Potato scripts/BlandPotatoController.cs
@@ -7,7 +7,7 @@ public class BlandPotatoController : MonoBehaviour
     new Rigidbody rb;
     //new Collider col;
     float cooldown = 3;
-    float force = 5f;
+    float force = 2f;
     Vector3 direction = new Vector3(-1,0,0); 
     // Start is called before the first frame update
     void Start()
