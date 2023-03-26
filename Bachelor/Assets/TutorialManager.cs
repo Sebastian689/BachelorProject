6547b79cc1a75f95592c9d9c8f734d6aa06d179f
Seb <sechr17@student.sdu.dk>
2023-03-26 16:43:35 +0200

    TutorialV1

---
+++ Bachelor/Assets/TutorialManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    [SerializeField]
+    bool engelsk = true;
+    GameObject DanskTutorial;
+    GameObject EngelskTutorial;
+    GameObject Panel;
+
+    private void Start()
+    {
+
+        Panel = GameObject.FindGameObjectWithTag("Panel");
+        Panel.SetActive(false);
+
+        EngelskTutorial = GameObject.FindGameObjectWithTag("EngelskTutorial");
+        
+        DanskTutorial = GameObject.FindGameObjectWithTag("DanskTutorial");
+        DanskTutorial.SetActive(false);
+    }
+    public void OnUnderstoodButton()
+    {
+        GameObject.FindGameObjectWithTag("Tutorial").SetActive(false);
+    }
+
+    public void OnTranslateButton()
+    {
+        if (engelsk)
+        {
+            EngelskTutorial.SetActive(false);
+            DanskTutorial.SetActive(true);
+            engelsk = false;
+        }
+        else if (!engelsk)
+        {
+            DanskTutorial.SetActive(false);
+            EngelskTutorial.SetActive(true);
+            engelsk = true;
+        }
+        
+    }
+}
