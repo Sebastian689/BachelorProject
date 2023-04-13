d5e82fb7ca3b82f10d0110f1c903d5a4757670f3
Simon Ebbesen Asmussen <siasm20@student.sdu.dk>
2023-04-13 12:19:08 +0200

    extra save

--- Bachelor/Assets/Scripts/Potato scripts/BlandPotatoController.cs
+++ Bachelor/Assets/Scripts/Potato scripts/BlandPotatoController.cs
@@ -16,7 +16,7 @@ public class BlandPotatoController : MonoBehaviour
     public GameManager GM;
     public GameObject go;
 
-
+ 
     public SoundManager soundManager;
     // Start is called before the first frame update
     void Start()
