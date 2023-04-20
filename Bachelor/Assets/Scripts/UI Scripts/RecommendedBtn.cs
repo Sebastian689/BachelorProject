c3fdb845e1a7c55844e02462d4493c951456c3b3
Simon Ebbesen Asmussen <siasm20@student.sdu.dk>
2023-04-20 18:27:10 +0200

    Made UI changes and all dat

---
+++ Bachelor/Assets/Scripts/UI Scripts/RecommendedBtn.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class RecommendedBtn : MonoBehaviour
+{
+    public Button yourButton;
+ 
+    public GameObject embeddedObject;
+    
+    public GameObject boost;
+    public GameObject revBoost;
+    public GameObject spring;
+    public GameObject brake;
+    public GameObject block;
+    public GameObject blockL;
+    public GameObject BlockR;
+    public GameObject ramp;
+    public GameObject revRamp;
+
+    [SerializeField] private GameObject GBS;
+    [SerializeField] private GridBuildingSystem gridSystem;
+    private string objectToPlace;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GBS = GameObject.FindGameObjectWithTag("Grid");
+        gridSystem = GBS.GetComponent<GridBuildingSystem>();
+
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+
+        switch (SceneManager.GetActiveScene().name)
+        {
+            case ("Level1"):
+                this.gameObject.SetActive(false);
+                break;
+        }
+    }
+
+    private void Awake()
+    {
+        if (SceneManager.GetActiveScene().name == "Level1")
+        {
+
+            this.gameObject.SetActive(false);
+
+        }
+        else {
+
+            /* switch (SceneManager.GetActiveScene().name)
+             {
+                 case ("Level2"):
+                     //run logic
+                     embeddedObject = ChooseLowest(spring, block, brake);
+                 break;
+                 case ("Level3"):
+                     //run logic
+                     embeddedObject = ChooseLowest(blockL, revBoost, revRamp);
+                 break;
+                 case ("Level4"):
+                     //run logic
+                     embeddedObject = ChooseLowest(ramp, block, revBoost);
+                 break;
+                 case ("Level5"):
+                     //run logic
+                     embeddedObject = ChooseLowest(blockL, blockR, ramp);
+                 break;
+             }
+            */
+        }
+
+    }
+
+    void TaskOnClick()
+    {
+        Debug.Log("You have clicked the button!");
+
+
+
+
+        //Activate placement
+
+        if (embeddedObject != null)
+        {
+            gridSystem.testTransform = embeddedObject.transform;
+
+            Debug.Log(embeddedObject);
+        }
+
+     
+    }
+
+    void ChooseLowest(GameObject one, GameObject two, GameObject three)
+    {
+
+       // int uno = one.int;
+       // if 
+    }
+
+}
