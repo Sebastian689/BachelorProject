4058f1aade8234e37b55c069c1d946113c52a60e
Sebastian <sechr17@student.sdu.dk>
2023-05-02 10:57:05 +0200

    fixed bug

--- Bachelor/Assets/BlockPrint.cs
+++ Bachelor/Assets/BlockPrint.cs
@@ -7,6 +7,7 @@ public class BlockPrint : MonoBehaviour
 {
     public Button yourButton;
     public GameObject Blockprint;
+    [SerializeField] bool hasRecommended;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,10 +17,16 @@ public class BlockPrint : MonoBehaviour
 
     void TaskOnClick()
     {
+        if (hasRecommended)
+        {
+            Blockprint.SetActive(false);
+            hasRecommended = false;
+        }
         if (Blockprint.activeInHierarchy)
         {
             Blockprint.transform.GetChild(0).gameObject.SetActive(false);
             Blockprint.transform.GetChild(1).gameObject.SetActive(true);
+            hasRecommended = true;
         }
     }
 }
--- Bachelor/Assets/SpringPrint.cs
+++ Bachelor/Assets/SpringPrint.cs
@@ -7,6 +7,7 @@ public class SpringPrint : MonoBehaviour
 {
     public Button yourButton;
     public GameObject Springprint;
+    [SerializeField] bool hasRecommended;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,10 +17,16 @@ public class SpringPrint : MonoBehaviour
 
     void TaskOnClick()
     {
+        if (hasRecommended)
+        {
+            Springprint.SetActive(false);
+            hasRecommended = false;
+        }
         if (Springprint.activeInHierarchy)
         {
             Springprint.transform.GetChild(0).gameObject.SetActive(false);
             Springprint.transform.GetChild(1).gameObject.SetActive(true);
+            hasRecommended = true;
         }
     }
 }
